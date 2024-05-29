94a913c21722c57bb79857548916e4fb80d1368b
JakubCe15 <162459302+JakubCe15@users.noreply.github.com>
2024-05-29 11:23:18 +0000

    LAB5_Commit4_4

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,29 +1,34 @@
 public class Student {
-
-  private String Name;
-  private String Surname;
-  private int Age;
-  private String dateOfBirth;
-
-  public Student(String name, String surname,int age) {
-    Name = name;
-    Age = age;
-    Surname = surname;
-  }
-
-  public String GetName() {return Name;}
-  public String GetSurname() {return Surname;}
-  public int GetAge() {return Age;}
-
-  public String ToString() {
-    return Name + " " + Surname + " " + Integer.toString(Age);
-  }
-  
-  public static Student Parse(String str) {
-    String[] data = str.split(" ");
-    if(data.length != 3){
-      return new Student("Parse Error", "Parase Error", -1);
+    private String Name;
+    private String Surname;
+    private int Age;
+    private String dateOfBirth;
+    public Student(String name, String surname, int age, String dateOfBirth) {
+        Name = name;
+        Surname = surname;
+        Age = age;
+        this.dateOfBirth = dateOfBirth;
     }
-    return new Student(data[0],data[1],Integer.parseInt(data[2]));
-  }
+    public String GetName() {
+        return Name;
+    }
+    public String GetSurname() {
+        return Surname;
+    }
+    public int GetAge() {
+        return Age;
+    }
+    public String GetDateOfBirth() {
+        return dateOfBirth;
+    }
+    public String ToString() {
+        return Name + " " + Surname + " " + Integer.toString(Age) + " " + dateOfBirth;
+    }
+    public static Student Parse(String str) {
+        String[] data = str.split(" ");
+        if (data.length != 4) {
+            return new Student("Parse Error", "Parse Error", -1, "Parse Error");
+        }
+        return new Student(data[0], data[1], Integer.parseInt(data[2]), data[3]);
+    }
 }
